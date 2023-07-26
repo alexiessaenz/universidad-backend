e32e9a337888095366ead2e3158e4f433a4bc26f
moises <00400815@uca.edu.sv>
2023-07-25 23:08:24 -0600

    feat: generics methods controller generic

--- src/main/java/com/springsimplespasos/universidad/universidadbackend/controlador/GenericController.java
+++ src/main/java/com/springsimplespasos/universidad/universidadbackend/controlador/GenericController.java
@@ -2,9 +2,14 @@ package com.springsimplespasos.universidad.universidadbackend.controlador;
 
 import com.springsimplespasos.universidad.universidadbackend.exception.BadRequestException;
 import com.springsimplespasos.universidad.universidadbackend.servicios.contratos.GenericoDAO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.HttpStatus;
 
 import java.util.List;
+import java.util.Optional;
 
 public class GenericController <E, S extends GenericoDAO<E>> {
 
@@ -16,17 +21,31 @@ public class GenericController <E, S extends GenericoDAO<E>> {
     }
 
     @GetMapping
-    public List<E> obtenerTodos(){
+    public ResponseEntity<List<E>> obtenerTodos(){
         List<E> listado = (List<E>) service.findAll();
         if(listado.isEmpty()) {
             throw new BadRequestException(String.format("No se han encontrado %ss", nombreEntidad));
         }
-        return listado;
+        return new ResponseEntity<>(listado, HttpStatus.OK);
+    }
+    @GetMapping(value = "/get/{id}")
+    public ResponseEntity<E> obtenerPorID(@PathVariable(value = "id") Integer id){
+        Optional<E> oEntidad = service.findById(id);
+        if(!oEntidad.isPresent()) {
+            throw new BadRequestException(String.format("%s con id %d no existe", nombreEntidad, id));
+        }
+        return new ResponseEntity<>(oEntidad.get(), HttpStatus.OK);
+    }
+    @DeleteMapping(value = "/delete/{id}")
+    public ResponseEntity<E> borrarPorID(@PathVariable(value = "id") Integer id){
+        Optional<E> oEntidad = service.findById(id);
+        if(!oEntidad.isPresent()) {
+            throw new BadRequestException(String.format("%s con id %d no existe", nombreEntidad, id));
+        }
+        service.deleteById(id);
+        return new ResponseEntity<>(oEntidad.get(), HttpStatus.OK);
     }
 
-    //obtenerPorID (Id)
-
-    //borrarEntidadPorId (Id)
 
     //altaEntidad (Entidad)
 }
